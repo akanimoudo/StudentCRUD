ca59edff8ce57aa769a24a1b0b21d28d65d87ba6
akanimoudo <akanimo17@gmail.com>
2024-05-07 15:34:34 +0100

    connected project to database

---
+++ dbcon.php
@@ -0,0 +1,16 @@
+<?php
+
+define('HOSTNAME','localhost');
+define('USERNAME','root');
+define('PASSWORD','');
+define('DATABASE','student crud db');
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+if ($connection === FALSE){
+echo 'Connection failed';
+}
+else{
+die ("Connection succeeded");
+}
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome To index.php</h2>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <td> 1</td>
+                    <td> Esther</td>
+                    <td>Asuquo </td>
+                    <td> 22</td>
+                </tr>
+                <tr>
+                    <td> 2</td>
+                    <td> Peace</td>
+                    <td> Jacob</td>
+                    <td> 14</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
